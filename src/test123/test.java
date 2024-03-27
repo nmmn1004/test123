fcff0095678c9ab945c372e49c169624a8cb4038
nmmn1004 <jjj4120@gmail.com>
2024-03-27 13:31:52 +0900

    c

--- src/test123/test.java
+++ src/test123/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("eclipse 불매운동");
+		System.out.println("eclipse 불매운동");
+		System.out.println("eclipse 불매운동");
 	}
 
 }
